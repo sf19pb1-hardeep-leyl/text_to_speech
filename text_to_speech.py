c0ed0532b9a5e1f8aeea18b62cafc3558fcb0ad6
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-09-11 11:39:22 -0400

    Create text_to_speech.py

---
+++ text_to_speech.py
@@ -0,0 +1,74 @@
+# Converts text to speech in different languages
+from gtts import gTTS
+import os
+
+language_code = """
+Language Code
+-------- ----
+Afrikaans       af
+Albanian        sq
+Arabic  ar
+Belarusian      be
+Bulgarian       bg
+Catalan         ca
+Chinese Simplified      zh-CN
+Chinese Traditional     zh-TW
+Croatian        hr
+Czech   cs
+Danish  da
+Dutch   nl
+English         en
+Estonian        et
+Filipino        tl
+Finnish         fi
+French  fr
+Galician        gl
+German  de
+Greek   el
+Hebrew  iw
+Hindi   hi
+Hungarian       hu
+Icelandic       is
+Indonesian      id
+Irish   ga
+Italian         it
+Japanese        ja
+Korean  ko
+Latvian         lv
+Lithuanian      lt
+Macedonian      mk
+Malay   ms
+Maltese         mt
+Norwegian       no
+Persian         fa
+Polish  pl
+Portuguese      pt
+Romanian        ro
+Russian         ru
+Serbian         sr
+Slovak  sk
+Slovenian       sl
+Spanish         es
+Swahili         sw
+Swedish         sv
+Thai    th
+Turkish         tr
+Ukrainian       uk
+Vietnamese      vi
+Welsh   cy
+Yiddish         yi
+"""
+
+print("We're going to translate anything you type into another language")
+mytext = input("Please enter some text in English: "
+print(language_code)
+language = input("Please select the language code: ")
+
+# Passing the text and language to the engine
+myobj = gTTS(text=mytext, lang=language, slow=True)
+
+# Saving the converted audio in a mp3 file named texty
+myobj.save("texty.mp3")
+
+# Playing the converted file
+os.system("mpg321 texty.mp3")
