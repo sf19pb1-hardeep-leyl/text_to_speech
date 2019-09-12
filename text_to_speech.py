e5e43a58f44635c5dc35b10076819022f09458fd
hardeep-leyl <54153538+hardeep-leyl@users.noreply.github.com>
2019-09-11 20:49:58 -0400

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,4 +1,4 @@
-# Converts text to speech in different languages. Requires pip3 install gTTS
+# Converts text to speech in different accents. Requires pip3 install gTTS
 from gtts import gTTS
 import os
 
@@ -59,10 +59,10 @@ Welsh   cy
 Yiddish         yi
 """
 
-print("We're going to translate anything you type into another language")
+print("We're going to speak anything you type in a different accent")
 mytext = input("Please enter some text in English: ")
 print(language_code)
-language = input("Please select the language code: ")
+language = input("Please select the accent: ")
 
 # Passing the text and language to the engine
 myobj = gTTS(text=mytext, lang=language, slow=True)
@@ -70,5 +70,6 @@ myobj = gTTS(text=mytext, lang=language, slow=True)
 # Saving the converted audio in a mp3 file named texty
 myobj.save("texty.mp3")
 
-# Playing the converted file.  It does create the file but doesnt play.  It also doesn't translate, just says it in a different accent!
+# It does create the file but doesnt play. 
+# Also, I wanted it to actually translete to a different language, but all it does is say it in a different accent!
 os.system("mpg321 texty.mp3")
